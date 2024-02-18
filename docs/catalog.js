eb3e206a8d882e5697f5ce35f1adc92edd91e536
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2024-02-18 05:25:19 -0600

    Fix script link for catalog page

--- docs/catalog.js
+++ docs/catalog.js
@@ -30,7 +30,7 @@ courses.forEach(c => {
     li.style.backgroundColor = `#${color}`;
 
     let hours = c.minHours === c.maxHours ?
-                `c.minHours ${c.minHours === 1 ? "credit" : "credits"}` :
+                `${c.minHours} ${c.minHours === 1 ? "credit" : "credits"}` :
                 `${c.minHours} to ${c.maxHours} credits`;
 
     li.innerHTML =
@@ -40,50 +40,3 @@ courses.forEach(c => {
 
     cardList.appendChild(li);
 });
-
-// const table = document.getElementById("classTable");
-// const tbody = table.getElementsByTagName("tbody")[0];
-
-// function loadJSON(path) {
-//     let xobj = new XMLHttpRequest();
-//     xobj.overrideMimeType("application/json");
-//     xobj.open("GET", path, async=false);
-//     xobj.send(null);
-//     return JSON.parse(xobj.responseText);
-// }
-
-// const classes = loadJSON("https://ujan-r.github.io/registration/classes.json");
-
-// let seen = Object.create(null);
-// const courses = classes.filter(c => {
-//     let key = `${c.subject} ${c.code}`;
-//     if (!seen[key]) {
-//         seen[key] = true;
-//         return true;
-//     }
-// })
-
-// let visibleCourses = courses;
-
-
-// function appendRow(subject, code, title) {
-//     let row = tbody.insertRow(table.rows.length - 1);
-//     let cells = [row.insertCell(), row.insertCell(), row.insertCell()];
-
-//     cells[0].appendChild(document.createTextNode(subject));
-//     cells[1].appendChild(document.createTextNode(code));
-//     cells[2].appendChild(document.createTextNode(title));
-// }
-
-// function updateTable() {
-//     clearTable();
-//     visibleCourses.forEach(c => {
-//         appendRow(c.subject, c.code, c.title);
-//     });
-// }
-
-// function clearTable() {
-//     tbody.innerHTML = "";
-// }
-
-// updateTable();
